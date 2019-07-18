05becdf2aa6f562a036a0de99808ceac4c4db597
zdw <dewei.zhou@xiaobao100.com>
2019-07-18 17:09:45 +0800

    通过绝对路径 对比resource字段是否一致，获取直接修改的modules

--- APlugin.js
+++ APlugin.js
@@ -1,5 +1,17 @@
 const pluginName = 'APlugin';
-
+var path = require('path')
+//改动的文件
+let diffFiles = ['/src/A1.js']
+let absDiffFiles = diffFiles.map(file => path.join(__dirname, file))
+console.log(__dirname)
+/**
+ * 
+ * @param {*} absDiffFiles 
+ * @param {*} modules 
+ */
+function compareAbsPath(absDiffFiles, modules) {
+    return modules.filter(singleModule => absDiffFiles.some(s => s === singleModule.resource))
+}
 class APlugin {
     apply(compiler) {
         compiler.hooks.compilation.tap(pluginName, compilation => {
@@ -7,7 +19,9 @@ class APlugin {
             /**不是'html-webpack-plugin'引起的入口 */
             if (!(compilation.name && compilation.name.includes('html-webpack-plugin'))) {
                 compilation.hooks.finishModules.tap(pluginName, modules => {
-                    console.log("finishModules");
+                    //直接改动的module
+                    let diffModules = []
+                    diffModules = compareAbsPath(absDiffFiles, modules)
                 });
             }
 
